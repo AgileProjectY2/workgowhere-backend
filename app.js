0e0d7682fa084d451603f5626ebfa3dbee7b662f
triciachow0212 <suyeechow0212@gmail.com>
2022-07-26 18:09:36 +0800

    Basic server and database running

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
+const database = mongoose.connection;
+require("dotenv").config();
+const mongoString = process.env.MONGODB_URI;
 
 app.use(express.json());
 app.use(cors());
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server is running...");
+});
+
+// Connection to mongoDB
+mongoose.connect(mongoString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Connect to DB and throw any error if connection fails
+database.on("error", err => {
+  console.log(`Unable to connect to database. ${err}`);
+});
+// .once will only run one time
+database.once("connected", () => {
+  console.log("Database connected!");
 });

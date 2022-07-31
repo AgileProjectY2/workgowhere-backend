986ad08b98593fade7d386d6f7ae5a7fb9baf3b5
triciachow0212 <suyeechow0212@gmail.com>
2022-07-31 23:50:38 +0800

    Create database schemas

--- models/listing.model.js
+++ models/listing.model.js
@@ -1,8 +1,67 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const ESTATES_SG = require("../utils/estates");
+const formateDate = require("../utils/formateDate");
 
 const listingSchema = new Schema({
-  //
+  name: {
+    type: String,
+    unique: false,
+    required: true,
+  },
+  description: {
+    type: String,
+    unique: false,
+    required: true,
+  },
+  address: {
+    streetOne: { type: String, required: true },
+    streetTwo: { type: String, required: false },
+    unitNum: { type: String, required: false },
+    postalCode: { type: String, required: true },
+  },
+  operatingHours: {
+    start: { type: String, required: true },
+    end: { type: String, required: true },
+    daysClosed: [{ type: String, required: true }],
+  },
+  contactNum: {
+    type: String,
+    required: true,
+  },
+  estate: {
+    type: String,
+    enum: ESTATES_SG,
+    required: true,
+  },
+  website: {
+    type: String,
+    required: true,
+  },
+  reservationUrl: {
+    type: String,
+    required: false,
+  },
+  keywords: [
+    {
+      type: String,
+    },
+  ],
+  images: {
+    type: String,
+    required: true,
+  },
+  listingOwner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: String,
+    default: formateDate,
+    immutable: true,
+    required: true,
+  },
 });
 
 // Solidify this schema as a data model with mongoose
--- models/user.model.js
+++ models/user.model.js
@@ -1,8 +1,38 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const formateDate = require("../utils/formateDate");
 
 const userSchema = new Schema({
-  //
+  fullName: {
+    type: String,
+    unique: false,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    unique: false,
+    required: true,
+  },
+  listingsPosted: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Listing",
+      required: true,
+    },
+  ],
+  createdAt: {
+    type: String,
+    default: formateDate,
+    immutable: true,
+    required: true,
+  },
 });
 
 // Solidify this schema as a data model with mongoose

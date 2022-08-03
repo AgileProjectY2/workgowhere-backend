96de5cd3f5a6cce719727958e4142b9d27660e9a
triciachow0212 <suyeechow0212@gmail.com>
2022-08-04 01:14:35 +0800

    Fetch user data from database for dashboard

--- controllers/privateController.js
+++ controllers/privateController.js
@@ -1,6 +1,21 @@
+const User = require("../models/userModel");
+
 // Fetch all listing(s) by a user id
-const user_dashboard = (req, res) => {
-  //
+const user_dashboard = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    let user = await User.findById({ _id: id });
+    user = await user.populate("listingsPosted");
+
+    if (res.status(200)) {
+      res.send({ message: "Data fetch successfully", user });
+    }
+  } catch (err) {
+    res.send({
+      message: "Error fetching data",
+    });
+  }
 };
 
 // Create a new listing by a user id

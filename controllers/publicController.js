5ea6a48f7ddd54f543fedce4ba251d4d6e62950b
triciachow0212 <suyeechow0212@gmail.com>
2022-08-03 23:55:06 +0800

    Register new accounts to database

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -1,6 +1,27 @@
+const User = require("../models/userModel.js");
+const bcrypt = require("bcrypt");
+
 // Register a new user
-const account_register = (req, res) => {
-  //
+const account_register = async (req, res) => {
+  const inputPassword = req.body.password;
+
+  try {
+    // Hash password
+    let hash = await bcrypt.hash(inputPassword, 10);
+    let newUser = new User({ ...req.body, password: hash });
+    // Save data in database
+    newUser = await newUser.save();
+
+    if (res.status(200)) {
+      res.send({ message: "Account created successfully", newUser });
+    }
+  } catch (err) {
+    if (err.code === 11000) {
+      res.send({ message: "Email already exists", err });
+    } else {
+      res.send({ message: "Error creating account" });
+    }
+  }
 };
 
 // Login a user

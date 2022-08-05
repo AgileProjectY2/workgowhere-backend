ffecef4cda434ebce707f97f449a100d1d26c691
Austinliannn <Austinliannn@gmail.com>
2022-08-05 17:10:16 +0800

    test
    -code testing

--- controllers/resultsController.js
+++ controllers/resultsController.js
@@ -1,11 +1,11 @@
 // Fetch results based on estate
 const results_estate = (req, res) => {
-  //
+  res.send("This is the results/estate page");
 };
 
 // Fetch results based on keywords/filters
 const results_keywords = (req, res) => {
-  //
+  res.send("This is the results/keywords page");
 };
 
 module.exports = {
